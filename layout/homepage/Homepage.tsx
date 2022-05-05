0ae7bf387fda709ae70ca8258b4c40d21df5bcb1
Elizabeth bricout <elizabeth.bricout@icloud.com>
2022-05-06 00:17:36 +0200

    Revert "stick footer bottom"
    This reverts commit 88860bc2d8f38f6457aedd8fc7584dc2f4caa232.

--- layout/homepage/Homepage.tsx
+++ layout/homepage/Homepage.tsx
@@ -7,20 +7,22 @@ import MainPart from '../../components/sections/homepage/MainPart';
 export default function Homepage(props) {
   const {githubRepositories} = props;
   return (
-    <Container maxWidth="lg" sx={{width: '80%'}}>
-      <MainPart />
-      <GithubCommits githubRepositories={githubRepositories} />
-      <Stack margin={10} justifyContent="center" alignItems="center">
-        <Link href="#" passHref>
-          <Button
-            variant="contained"
-            sx={{borderRadius: 8}}
-            endIcon={<ArrowForwardIos />}
-          >
-            About me
-          </Button>
-        </Link>
-      </Stack>
-    </Container>
+    <Box height="100%">
+      <Container maxWidth="lg" sx={{width: '80%'}}>
+        <MainPart />
+        <GithubCommits githubRepositories={githubRepositories} />
+        <Stack margin={10} justifyContent="center" alignItems="center">
+          <Link href="#" passHref>
+            <Button
+              variant="contained"
+              sx={{borderRadius: 8}}
+              endIcon={<ArrowForwardIos />}
+            >
+              About me
+            </Button>
+          </Link>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
