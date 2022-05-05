88860bc2d8f38f6457aedd8fc7584dc2f4caa232
Elizabeth bricout <elizabeth.bricout@icloud.com>
2022-05-06 00:10:16 +0200

    stick footer bottom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import {CacheProvider, EmotionCache} from '@emotion/react';
-import {CssBaseline, Stack} from '@mui/material';
+import {CssBaseline, GlobalStyles, Stack} from '@mui/material';
 import {AppProps} from 'next/app';
 import Head from 'next/head';
 import * as React from 'react';
@@ -18,6 +18,11 @@ interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
 
+  React.useEffect(() => {
+    const tar = document.getElementById('__next');
+    tar.style.height = '100%';
+  }, []);
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -27,7 +32,10 @@ export default function MyApp(props: MyAppProps) {
       <DynamicThemeProvider>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Stack height="100vh">
+        <GlobalStyles
+          styles={{html: {height: '100%'}, body: {height: '100%'}}}
+        />
+        <Stack minHeight="100vh" height={'100%'}>
           <Nav />
           <Component {...pageProps} />
           <Footer />
